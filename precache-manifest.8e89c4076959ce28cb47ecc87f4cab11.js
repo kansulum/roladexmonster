7fbce275f7646bc079c695c610e4a087ee16be3b
Abdulraman Mohammed <abdulraman.mohammed@cwg-plc.com>
2019-11-16 10:13:56 +0100

    Updates

---
+++ precache-manifest.8e89c4076959ce28cb47ecc87f4cab11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b3badc972ddeaa2bbff6b80f84c93a",
+    "url": "/roladexmonster/index.html"
+  },
+  {
+    "revision": "7278e755fc03db402366",
+    "url": "/roladexmonster/static/css/main.1eb96f0b.chunk.css"
+  },
+  {
+    "revision": "331a6c9f26a1f158523b",
+    "url": "/roladexmonster/static/js/2.f641515a.chunk.js"
+  },
+  {
+    "revision": "7278e755fc03db402366",
+    "url": "/roladexmonster/static/js/main.d3483741.chunk.js"
+  },
+  {
+    "revision": "a366733c9bcb47a74f1f",
+    "url": "/roladexmonster/static/js/runtime-main.61dda80d.js"
+  }
+]);
